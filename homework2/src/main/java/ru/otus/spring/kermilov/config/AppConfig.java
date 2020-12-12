87ff295d4a180848ea845b60d5a1b12ad009b3b2
kermilov <xharderx@yandex.ru>
2020-12-12 23:13:41 +0300

    homework2. Файл настроек для приложения тестирования студентов

--- homework2/src/main/java/ru/otus/spring/kermilov/config/AppConfig.java
+++ homework2/src/main/java/ru/otus/spring/kermilov/config/AppConfig.java
@@ -3,14 +3,16 @@ package ru.otus.spring.kermilov.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import ru.otus.spring.kermilov.dao.CSVQuestionDAO;
 import ru.otus.spring.kermilov.dao.CSVQuestionDAOImpl;
 import ru.otus.spring.kermilov.service.QAServiceImpl;
 
+@PropertySource("classpath:application.properties")
 @Configuration
 public class AppConfig {
     @Bean
-    CSVQuestionDAO QADao(@Value("questions.csv") String csvPath) {
+    CSVQuestionDAO QADao(@Value("${csv.name}") String csvPath) {
         return new CSVQuestionDAOImpl(csvPath);
     }
 
