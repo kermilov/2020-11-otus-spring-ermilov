2b754dead9db7a50df894457940ff252756eaf5f
kermilov <kermilov@diasoft.ru>
2021-03-15 23:57:27 +0300

    homework10. константы, для порядка

--- homework10/src/main/java/ru/otus/spring/kermilov/books/controller/BookCommentController.java
+++ homework10/src/main/java/ru/otus/spring/kermilov/books/controller/BookCommentController.java
@@ -10,33 +10,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class BookCommentController {
 
+    private static final String VIEW_COMMENT_T = "comment-table";
+    private static final String VIEW_COMMENT_F = "comment-form";
+    private static final String ATTR_BOOK_COMMENT_ID = "bookCommentId";
+    private static final String ATTR_BOOK_ID = "bookId";
+    private static final String ATTR_ACTION = "action";
+
     @GetMapping("/comment")
     public String listPage(@RequestParam("id") long id, Model model) {
-        model.addAttribute("bookId", id);
-        return "comment-table";
+        model.addAttribute(ATTR_BOOK_ID, id);
+        return VIEW_COMMENT_T;
     }
 
     @GetMapping("/comment/insert")
     public String insertPage(@RequestParam("id") long id, Model model) {
-        model.addAttribute("bookCommentId", 0);
-        model.addAttribute("bookId", id);
-        model.addAttribute("action", 1);
-        return "comment-form";
+        model.addAttribute(ATTR_BOOK_COMMENT_ID, 0);
+        model.addAttribute(ATTR_BOOK_ID, id);
+        model.addAttribute(ATTR_ACTION, 1);
+        return VIEW_COMMENT_F;
     }
 
     @GetMapping("/comment/edit")
     public String editPage(@RequestParam("id") long id, Model model) {
-        model.addAttribute("bookCommentId", id);
-        model.addAttribute("bookId", id);
-        model.addAttribute("action", 2);
-        return "comment-form";
+        model.addAttribute(ATTR_BOOK_COMMENT_ID, id);
+        model.addAttribute(ATTR_BOOK_ID, id);
+        model.addAttribute(ATTR_ACTION, 2);
+        return VIEW_COMMENT_F;
     }
 
     @GetMapping("/comment/delete")
     public String deletePage(@RequestParam("id") long id, Model model) {
-        model.addAttribute("bookCommentId", id);
-        model.addAttribute("bookId", id);
-        model.addAttribute("action", 3);
-        return "comment-form";
+        model.addAttribute(ATTR_BOOK_COMMENT_ID, id);
+        model.addAttribute(ATTR_BOOK_ID, id);
+        model.addAttribute(ATTR_ACTION, 3);
+        return VIEW_COMMENT_F;
     }
 }
--- homework10/src/main/java/ru/otus/spring/kermilov/books/controller/BookController.java
+++ homework10/src/main/java/ru/otus/spring/kermilov/books/controller/BookController.java
@@ -10,30 +10,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class BookController {
 
+    private static final String VIEW_BOOK_T = "book-table";
+    private static final String VIEW_BOOK_F = "book-form";
+    private static final String ATTR_BOOK_ID = "bookId";
+    private static final String ATTR_ACTION = "action";
+
     @GetMapping("/")
     public String listPage() {
-        return "book-table";
+        return VIEW_BOOK_T;
     }
 
     @GetMapping("/book/insert")
     public String insertPage(Model model) {
-        model.addAttribute("bookId", 0);
-        model.addAttribute("action", 1);
-        return "book-form";
+        model.addAttribute(ATTR_BOOK_ID, 0);
+        model.addAttribute(ATTR_ACTION, 1);
+        return VIEW_BOOK_F;
     }
 
     @GetMapping("/book/edit")
     public String editPage(@RequestParam("id") long id, Model model) {
-        model.addAttribute("bookId", id);
-        model.addAttribute("action", 2);
-        return "book-form";
+        model.addAttribute(ATTR_BOOK_ID, id);
+        model.addAttribute(ATTR_ACTION, 2);
+        return VIEW_BOOK_F;
     }
 
     @GetMapping("/book/delete")
     public String deletePage(@RequestParam("id") long id, Model model) {
-        model.addAttribute("bookId", id);
-        model.addAttribute("action", 3);
-        return "book-form";
+        model.addAttribute(ATTR_BOOK_ID, id);
+        model.addAttribute(ATTR_ACTION, 3);
+        return VIEW_BOOK_F;
     }
 }
 
